155a32e32d9ce0f4a075fec9253345ee353d8e19
Rodrigo Fernandez <rodrigof3012@gmail.com>
2022-05-07 13:04:57 -0300

    Update Header.js

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,19 +1,9 @@
-import React, { useState, Fragment } from "react";
+import React, { Fragment } from "react";
 import style from "./Header.module.css";
 import { Link } from "react-router-dom";
 import logo from "./logo1.png";
 
 function Header() {
-  const [isActive, setActive] = useState("false");
-  const [input, setInput] = useState("");
-
-  function handleInput() {
-    setInput("");
-  }
-  function toggleClass() {
-    setActive(!isActive);
-  }
-
   return (
     <Fragment>
       <nav className={style.container}>
@@ -34,29 +24,9 @@ function Header() {
                 Movie Grid
               </Link>
             </li>
-            <li className={style.dropdownfirst} onClick={toggleClass}>
-              Genres
-            </li>
+            <li className={style.dropdownfirst}>Genres</li>
             <li className={style.dropdownfirst}>My list</li>
           </ul>
-        </div>
-        <div>
-          <form>
-            <input
-              className={style.buscador}
-              type="text"
-              placeholder="Search..."
-              onChange={(e) => setInput(e.target.value)}
-              value={input}
-            />
-            <Link to={`/Peliculagrid/`}>
-              <a href="#logohome">
-                <button onClick={handleInput} className={style.botonheader}>
-                  Search
-                </button>
-              </a>
-            </Link>
-          </form>
         </div>
       </nav>
     </Fragment>
